ab64bdcdeddb0683486077435e9cc73cff0b17e1
ignacio rolon <i.rolonmarecos@gmail.com>
2022-07-25 19:59:19 -0300

    Modificacion entrega

--- routes/account.js
+++ routes/account.js
@@ -2,39 +2,44 @@ const { json } = require('express');
 const express = require('express');
 const {Router} = express
 const Container = require('../conteiner')
-
-const app = express()
+const bodyParser = require('body-parser')
+const app = express();
 const router = Router();
 const totalProductos = new Container('./texto.json')
+app.use(bodyParser.json())
+
 
 app.use(express.json())
 
-router.get('/api/productos',async (req,res)=>{
-    let productos = await req.body
-    console.log(productos);
-    res.send(`${JSON.stringify(productos)}`)
+router.get('/',async (req,res)=>{
+    let productos = await totalProductos.getAll()
+    res.json(productos)
 })
 
-router.get('/api/productos/:id', async (req,res)=>{
+router.get('/:id', async (req,res)=>{
     const id = Number(req.params.id);
     const prodId = await totalProductos.getById(id)
     prodId?res.json(prodId):res.status(404).end()
 })
 
-router.delete('/api/productos/:id', async(req,res)=>{
+router.delete('/:id', async(req,res)=>{
     const id = Number(req.params.id);
     totalProductos.deleteById(id)
     res.status(204).end()
 })
 
-router.post('/api/productos',  (req,res)=>{
+router.post('/', async (req,res)=>{
     const nvoProd =  req.body
     console.log(nvoProd);
-    res.json(nvoProd);
+    const productoAgregado= await totalProductos.save(nvoProd)
+    //const pr =   json.nvoProd
+    //console.log(pr);
+    console.log(productoAgregado);
+    res.json(productoAgregado);
     //res.status(200).end()
 })
 
-router.put('/api/productos/:id', async (req,res)=>{
+router.put('/:id', async (req,res)=>{
     const id = Number(req.params.id);
     const {product, price} = req.body
     await totalProductos.update(id,product, price)
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
+const { use } = require('./routes/account')
 const app = express()
 
 const router = require('./routes/account')
-app.use(express.static('public'))
-
 
 app.use(express.json())
-app.use(router)
+app.use(express.urlencoded({extended: true}))
+app.use(express.static( 'public'))
+app.use('/api/productos',router)
 
 const PORT = process.env.PORT || 2020
 const server = app.listen(PORT, ()=>{
