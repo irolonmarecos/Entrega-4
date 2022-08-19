29fac0ed14b432e39d2fc991bc98b126ebe4d7ee
ignacio rolon <i.rolonmarecos@gmail.com>
2022-08-18 23:53:02 -0300

    trabajo

--- Utils/conteiner.js
+++ Utils/conteiner.js
@@ -10,38 +10,47 @@ function calcId (id,dataNuevo){
 }
 
 class Almacen  {
-    constructor(product, price, id){
-        this.product = product
-        this.price = price
-        this.id = id  
+    constructor(Nombre, Descripcion,price,img, id,date,stock,){
+        this.id = id,
+        this.Nombre = Nombre,
+        this.Descripcion=Descripcion
+        this.Price = price,
+        this.Img = img,
+        this.Date = date,
+        this.stock=stock
     }
 } 
 class container  {
     async getAll(){
         try{
-            const data =  fs.readFileSync("./routes/stock.json", "utf-8");
+            const data =  fs.readFileSync("./utils/stock.json", "utf-8");
             let dataNuevo = JSON.parse(data)
             return dataNuevo.Productos
         }catch(err){
             throw new error ('ERROR')
         }
     }
-    async save(nombre, precio){
+    async save(Nombre,Descripcion,price,img,stock){
         try{
-            const data = fs.readFileSync("./routes/stock.json", "utf-8")
+            const data =  fs.readFileSync("./utils/stock.json", "utf-8");
             const dataNuevo = JSON.parse(data)
             let Id = dataNuevo.Productos.length
-            const nvoProd = new Almacen (nombre,precio,Id)
+            const nvoProd = new Almacen (Id,Nombre,Descripcion,price,img,stock)
             nvoProd.id = calcId (nvoProd.id, dataNuevo)
-            dataNuevo.Productos.push(nvoProd)
-            fs.writeFileSync("./routes/stock.json", JSON.stringify(dataNuevo, null, 2))
+            const _nvoProd = {...nvoProd,
+                Date: Date.now(),
+                Code: Math.ceil(Math.random()*10000000000000),
+                
+            }
+            dataNuevo.Productos.push(_nvoProd)
+            fs.writeFileSync("./utils/stock.json", JSON.stringify(dataNuevo, null, 2))
         }catch(err){
             throw new error ('ERROR')
         }
     }
     async getById (id){
         try{
-            const data = fs.readFileSync("./routes/stock.json", "utf-8")
+            const data =  fs.readFileSync("./utils/stock.json", "utf-8");
             const dataNuevo = JSON.parse(data)
             let filtro = dataNuevo.Productos.find((el) => el.id === id)
             return filtro;
@@ -51,36 +60,36 @@ class container  {
     }
     async deleteById(id) {
         try{
-            const data = fs.readFileSync("./routes/stock.json", "utf-8")
+            const data =  fs.readFileSync("./utils/stock.json", "utf-8");
             const dataNuevo = JSON.parse(data)
             const {Productos} = dataNuevo
             let filtro = Productos.filter((el) => el.id !== id)
             dataNuevo.Productos = filtro
-            fs.writeFileSync("./routes/stock.json", JSON.stringify(dataNuevo, null, 2))
+            fs.writeFileSync("./utils/stock.json", JSON.stringify(dataNuevo, null, 2))
         }catch(err){
             throw new error ('ERROR')
         }
     } 
     async deleteAll() {
         try{
-            const data = fs.readFileSync("./routes/stock.json", "utf-8")
+            const data =  fs.readFileSync("./utils/stock.json", "utf-8");
             const dataNuevo = JSON.parse(data)
             const {Productos} = dataNuevo
             let borrado = []
             dataNuevo.Productos = borrado
-            fs.writeFileSync("./routes/stock.json", JSON.stringify(dataNuevo, null, 2))
+            fs.writeFileSync("./utils/stock.json", JSON.stringify(dataNuevo, null, 2))
         }catch(err){
             throw new error ('ERROR')
         }
     } 
     async update(id,prod,pric) {
         try{
-            const data = fs.readFileSync("./routes/stock.json", "utf-8")
+            const data =  fs.readFileSync("./utils/stock.json", "utf-8");
             const dataNuevo = JSON.parse(data)
             let filtro = dataNuevo.Productos.find((el) => el.id === id)
             filtro.product = prod;
             filtro.price =  pric;
-            fs.writeFileSync("./routes/stock.json", JSON.stringify(dataNuevo, null, 2))
+            fs.writeFileSync("./utils/stock.json", JSON.stringify(dataNuevo, null, 2))
         }catch(err){
             throw new error (err)
         }
@@ -97,4 +106,3 @@ const viewList = new container()
 //viewList.deleteById(4)
 //viewList.deleteAll()
 //viewList.getAll()
-
--- server.js
+++ server.js
@@ -1,16 +1,40 @@
-const express = require('express')
-const { use } = require('./routes/account')
-const app = express()
+const express = require('express');
 
-const router = require('./routes/account')
+const {Server:HTTPServer} = require('http');
+const {Server:SocketServer} = require('socket.io')
 
-app.use(express.json())
-app.use(express.static( 'public'))
+const app = express();
+const routerProductos = require('./router/productos');
+const routerCarrito = require('./router/carrito')
+const httpServer = new HTTPServer(app);
+const socketServer = new SocketServer(httpServer);
+const Contenedor = require('./utils/conteiner');
+const nvoCont = new Contenedor('./utils/stock.json')
+
+
+app.use(express.json());
+app.use(express.static( 'public'));
 app.use(express.urlencoded({extended:true}));
-app.use('/api/productos',router)
+app.use('/api/productos',routerProductos);
+app.use('/api/carrito',routerCarrito);
 
-const PORT = process.env.PORT || 8080
-const server = app.listen(PORT, ()=>{
-    console.log(`El servidor que se esta ejecutando es el ${PORT}`);
+app.set('view engine', 'pug');
+app.set('views', './views')
+
+
+socketServer.on('connection', async(socket)=>{
+    console.log('Nuevo cliente conectado');
+    const productos = await nvoCont.getAll();
+    socket.emit('MOSTRAR_PRODUCTOS',productos)
+
+
+    
 })
-server.on("error", error => `Error: ${error}`)
+
+
+
+const PORT = process.env.PORT || 2020
+httpServer.listen(PORT, ()=>{
+    console.log(`El servidor se esta ejecutando en el puerto ${PORT}`);
+})
+httpServer.on("error", error => `Error: ${error}`);
